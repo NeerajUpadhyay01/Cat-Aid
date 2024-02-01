c6662089c2d4cf577a28d8fe9300bba597f102a1
Neeraj <neerajsaraswat111@gmail.com>
2024-02-01 18:11:19 +0530

    All files were added

---
+++ app.js
@@ -0,0 +1,9 @@
+// document.addEventListener("scroll", function () {
+//   const scrollPercentage =
+//     (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+//   document.querySelectorAll(".card").forEach((card) => {
+//     const animatedElement = card.querySelector(".animated-element");
+//     animatedElement.style.opacity = Math.min(scrollPercentage / 100, 1);
+//     animatedElement.style.transform = `translate(-50%, -50%)`;
+//   });
+// });
